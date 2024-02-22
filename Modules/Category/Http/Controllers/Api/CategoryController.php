1e2f3ef95de6cd2336ee1f43ce0b651482d18080
Amir Vahedi <amirvahedix@gmail.com>
2024-02-23 00:38:24 +0330

    Created CategoryResource

--- Modules/Category/Http/Controllers/Api/CategoryController.php
+++ Modules/Category/Http/Controllers/Api/CategoryController.php
@@ -3,15 +3,18 @@
 namespace Modules\Category\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Modules\Category\Http\Resources\CategoryResource;
 use Modules\Category\Models\Category;
 
 class CategoryController extends Controller
 {
     public function index()
     {
-        return Category::query()
-            ->whereNull('parent_id')
-            ->with('children')
-            ->get();
+        return CategoryResource::collection(
+            Category::query()
+                ->whereNull('parent_id')
+                ->with('children')
+                ->get()
+        );
     }
 }
