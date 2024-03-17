fdfadafb2ba86ad86aa35aa306f22d74ebe7d07a
Amir Vahedi <amirvahedix@gmail.com>
2024-03-17 17:54:12 +0330

    Implemented Banner Filament Resource

--- Modules/Banner/Models/Banner.php
+++ Modules/Banner/Models/Banner.php
@@ -5,13 +5,15 @@ namespace Modules\Banner\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Modules\Banner\Database\factories\BannerFactory;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Banner extends Model
+class Banner extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
 
-    const TYPE_FULL_WIDTH = 'full_width';
-    const TYPE_HALF_WIDTH = 'half_width';
+    const TYPE_FULL_WIDTH = 'Full Width';
+    const TYPE_HALF_WIDTH = 'Half Width';
     const types = [self::TYPE_FULL_WIDTH, self::TYPE_HALF_WIDTH];
 
     protected $fillable = [
---
+++ app/Filament/Resources/BannerResource.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BannerResource\Pages;
+use App\Filament\Resources\BannerResource\RelationManagers;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Modules\Banner\Models\Banner;
+
+class BannerResource extends Resource
+{
+    protected static ?string $model = Banner::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-photo';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')->required(),
+                Forms\Components\TextInput::make('link')->required(),
+                Forms\Components\SpatieMediaLibraryFileUpload::make('image'),
+                Forms\Components\Select::make('type')
+                    ->options([
+                        Banner::TYPE_FULL_WIDTH => Banner::TYPE_FULL_WIDTH,
+                        Banner::TYPE_HALF_WIDTH => Banner::TYPE_HALF_WIDTH
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\SpatieMediaLibraryImageColumn::make('image'),
+                Tables\Columns\TextColumn::make('title'),
+                Tables\Columns\TextColumn::make('type'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBanners::route('/'),
+            'create' => Pages\CreateBanner::route('/create'),
+            'edit' => Pages\EditBanner::route('/{record}/edit'),
+        ];
+    }
+}
